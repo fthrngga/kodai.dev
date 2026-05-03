99600c2196fe3ee4e21f20f744f9ded5e32ab715
fthrngga <fathur22ti@mahasiswa.pcr.ac.id>
2026-05-03 17:51:20 +0700

    fix: force HTTPS and trust proxies for Railway deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // Trust Railway's reverse proxy so HTTPS is detected correctly
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
